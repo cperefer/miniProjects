a390d9c452ba9e4304af658c4c84facf658e1631
Miguel <migui.kaos14@gmail.com>
2026-05-03 10:30:42 +0200

    feat (dailyTracker): added new habit

--- apps/02_daily_habits_tracker/src/NewHabit.tsx
+++ apps/02_daily_habits_tracker/src/NewHabit.tsx
@@ -1,7 +1,50 @@
+import { useState } from "react";
+import { habitTypes } from "./utils/HabitTypes";
+
 export default function NewHabit() {
+  const [habit, setHabit] = useState("");
+  const [type, setType] = useState("9999");
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    console.log("click");
+    console.log({ habit, type });
+  };
+
   return (
-    <div>
-      <div>New Habit</div>
+    <div className="border border-border rounded-xl bg-white p-4">
+      <form className="flex justify-between">
+        <input
+          type="text"
+          value={habit}
+          className="w-100 border border-border rounded-xl p-2"
+          placeholder="Nuevo hábito..."
+          onChange={({ target }) => setHabit(target.value)}
+        />
+        <select
+          value={type}
+          onChange={({ target }) => setType(target.value)}
+          className="w-40 border border-border rounded-xl p-2 hover:border-border hover:cursor-pointer foucs:outline-none focus:border-border form-select appearance-none"
+        >
+          <option value="9999">Tipo</option>
+          {habitTypes.map(({ name, id }) => (
+            <option
+              key={id}
+              value={id}
+              className={`before:content-['·'] before:w-20 before:h-20`}
+            >
+              {/* <span className="block w-2 h-2 bg-amber-500"></span> */}
+              {name}
+            </option>
+          ))}
+        </select>
+        <button
+          onClick={handleClick}
+          className="bg-primary text-white p-3 px-5 rounded-md"
+        >
+          + Añadir
+        </button>
+      </form>
     </div>
   );
 }
