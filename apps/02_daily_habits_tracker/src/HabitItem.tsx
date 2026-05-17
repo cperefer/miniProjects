6e85268614899c3ccad486024ce1762414952c42
Miguel <migui.kaos14@gmail.com>
2026-05-17 19:42:45 +0200

    feat (dailyTracker): abstracted habit to HabitItem component

--- apps/02_daily_habits_tracker/src/HabitItem.tsx
+++ apps/02_daily_habits_tracker/src/HabitItem.tsx
@@ -1,7 +1,24 @@
-export default function HabitItem() {
+import HabitTypeIcon from "./HabitTypeIcon";
+
+export default function HabitItem({ name, color, icon, habitType }) {
   return (
-    <div>
-      <div>Habit Item</div>
+    <div className="flex flex-row w-1/2">
+      <div
+        className="rounded-full w-15 h-15 flex items-center justify-center"
+        style={{ backgroundColor: `${color}20` }}
+      >
+        <HabitTypeIcon icon={icon} color={color} />
+      </div>
+      <div className="flex flex-col justify-between pl-5">
+        <span className="font-bold">{name}</span>
+        <div className="flex">
+          <span
+            className="w-3 h-3 rounded-full block mt-1.5"
+            style={{ backgroundColor: color }}
+          ></span>
+          <span className="text-text-secondary pl-1">{habitType}</span>
+        </div>
+      </div>
     </div>
   );
 }
--- apps/02_daily_habits_tracker/src/HabitListItem.tsx
+++ apps/02_daily_habits_tracker/src/HabitListItem.tsx
@@ -1,4 +1,5 @@
-import HabitTypeIcon from "./HabitTypeIcon";
+import HabitCounter from "./HabitCounter";
+import HabitItem from "./HabitItem";
 import type { Habit } from "./models/habit";
 import { habitTypes } from "./utils/HabitTypes";
 
@@ -12,25 +13,15 @@ export default function HabitListItem({ habit }: { habit: Habit }) {
       className="flex flex-row h-20 p-3 gap-4 rounded-xl mb-3 border"
       style={{ backgroundColor: `${secondary}95`, borderColor: `${primary}20` }}
     >
-      <div className="flex flex-row w-1/2">
-        <div
-          className="rounded-full w-15 h-15 flex items-center justify-center"
-          style={{ backgroundColor: `${primary}20` }}
-        >
-          <HabitTypeIcon icon={habitType.icon} color={primary} />
-        </div>
-        <div className="flex flex-col justify-between pl-5">
-          <span className="font-bold">{habit.name}</span>
-          <div className="flex">
-            <span
-              className="w-3 h-3 rounded-full block mt-1.5"
-              style={{ backgroundColor: primary }}
-            ></span>
-            <span className="text-text-secondary pl-1">{habitType.name}</span>
-          </div>
-        </div>
-      </div>
-      <div>botones</div>
+      <HabitItem
+        name={habit.name}
+        color={primary}
+        habitType={habitType.name}
+        icon={habitType.icon}
+      />
+      <HabitCounter timesDone={habit.timesDone} color={primary} />
+      <div>reset</div>
+      <div>delete</div>
     </div>
   );
 }
